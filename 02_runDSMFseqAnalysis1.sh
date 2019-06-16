db4568dff95cfdfb3c2af189bb18dee8b500f6cb
jsemple19 <semple.j@gmail.com>
2019-06-16 14:56:56 +0200

    local changes

--- 02_runDSMFseqAnalysis1.sh
+++ 02_runDSMFseqAnalysis1.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=jennifer.semple@izb.unibe.ch
 #SBATCH --mail-type=end,fail
 #SBATCH --job-name="dSMF_Ccall"
-#SBATCH --time=2-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --partition=all
 #SBATCH --mem-per-cpu=64G
 
