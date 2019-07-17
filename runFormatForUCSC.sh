d081bb798ed0c419912d87f50d3ecc3e1100c53f
jsemple19 <semple.j@gmail.com>
2019-07-17 12:48:31 +0200

    reducing resource request for formatting script

--- runFormatForUCSC.sh
+++ runFormatForUCSC.sh
@@ -2,14 +2,14 @@
 
 ## Allocate resources
 #SBATCH --time=0-00:10:00
-#SBATCH --mail-user=jennifer.semple@izb.unibe.ch
-#SBATCH --mail-type=end,fail
+##SBATCH --mail-user=jennifer.semple@izb.unibe.ch
+##SBATCH --mail-type=end,fail
 #SBATCH --job-name="formatUCSC"
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=all
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=2G
 
 
 
-./formatForUCSC.sh gwDSMF16-20_Feb "Genome wide dSMF dS016 & dS020, Feb-2019" ./bigwig/*.bw
+./formatForUCSC.sh gwDSMFv16v20_Comb "Genome wide dSMF dS016 & dS020, Feb & May 2019 combined" ./bigwig/*.bw
 
